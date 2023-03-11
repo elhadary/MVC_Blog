f0baf98bcf2b89d9e9921ccb6dbd052d3e87023b
elhadary <fares30977003@commerce.tanta.edu.eg>
2023-03-11 12:39:54 +0200

    -Validation Class -Rendering alerts -Completed AuthController

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -4,9 +4,11 @@ namespace app\Controllers;
 
 use app\Core\Controller;
 use app\Models\User;
+
 class UserController extends Controller
 {
     public User $user;
+
     public function __construct()
     {
         parent::__construct();
